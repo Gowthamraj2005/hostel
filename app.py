df7dccc868dea0ef01a2b35deaf650e6734e6d43
Gowthamraj2005 <144456072+Gowthamraj2005@users.noreply.github.com>
2026-02-22 10:50:39 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import json 
 from flask import Flask, request, render_template, redirect
 from twilio.rest import Client
 from datetime import datetime
 import os
 
 app = Flask(__name__)
+def save_to_google_sheets(data):
+
+    creds_json = json.loads(os.environ.get("GOOGLE_CREDENTIALS"))
+
+    credentials = Credentials.from_service_account_info(
+        creds_json,
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ],
+    )
+
+    gc = gspread.authorize(credentials)
+
+    sheet = gc.open("Hostel Leave Records").sheet1
+
+    sheet.append_row(data)
 
 # ---------------- TWILIO CONFIG ----------------
 account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
@@ -39,15 +59,15 @@ def home():
 @app.route("/approve", methods=["POST"])
 def approve():
 
-    name = request.form["name"]
-    room = request.form["room"]
-    reason = request.form["reason"]
-    start = request.form["start"]
-    end = request.form["end"]
-    days = request.form["days"]
-    father = request.form["father"]
-    principal = request.form["principal"]
-    student = request.form["student"]
+    name = request.form.get("name")
+    room = request.form.get("room")
+    reason = request.form.get("reason")
+    start = request.form.get("start")
+    end = request.form.get("end")
+    days = request.form.get("days")
+    father = request.form.get("father")
+    principal = request.form.get("principal")
+    student = request.form.get("student")
 
     message_body = f"""
 LEAVE APPROVED ✅
@@ -64,16 +84,32 @@ Approved by Warden.
 
     # Send WhatsApp
     for number in [father, principal, student]:
-        client.messages.create(
-            from_=twilio_number,
-            body=message_body,
-            to=f"whatsapp:{number}"
-        )
+        if number:
+            client.messages.create(
+                from_=twilio_number,
+                body=message_body,
+                to=f"whatsapp:{number}"
+            )
+
+    # ✅ Save to Google Sheets (INSIDE FUNCTION)
+    save_to_google_sheets([
+        name,
+        room,
+        reason,
+        days,
+        start,
+        end,
+        father,
+        principal,
+        student,
+        "Approved"
+    ])
 
     return redirect("/")
 
 
 if __name__ == "__main__":
     app.run()
+
 
 
